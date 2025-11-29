ad4abe5831178ea03fea8eb8aeb0e06ff2b38f0f
adcarrillo13 <a690c665@ku.edu>
2025-11-28 19:32:31 -0600

    added form page

---
+++ Manual/frontend/form.js
@@ -0,0 +1,17 @@
+import {useState} from ‘react’
+import axios from "axios"
+import { useNavigate } from 'react-router-dom'
+const CREATE_END = "http://localhost..."
+
+
+export function (){
+
+	}
+
+	return (
+		<div>
+
+		</div>
+	);
+
+}
