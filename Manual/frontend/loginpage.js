ae642f56ce5a45c09146dc53cfdb6c1095a897aa
adcarrillo13 <a690c665@ku.edu>
2025-11-28 18:50:59 -0600

    outline of login

---
+++ Manual/frontend/loginpage.js
@@ -0,0 +1,36 @@
+import {useState} from ‘react’
+import axios from "axios"
+coust CREATE_END = "http://localhost..."
+
+
+export function Login(tockenSetter){
+	const [name, setName] = useState("");
+	const [password, setPassword] = useState("");
+
+	const handleSubmit = async function(e){
+		e.preventDefault();
+		const params = {username:name, pw:password}
+		axios.post(CREATE_END, params)
+		alert("Creating " + name + " " + password);
+	}
+
+	return (
+		<div>
+		Login
+		<form onSubmit={handleSubmit}>
+		<p>Name 
+		<input type="text" onChange={function(e){ setName(e.target.value)}}>
+		</input>
+		</p>
+
+		<p>password
+		<input type="password" onChange={function(e){ setPassword(e.target.value)}}>
+		</input>
+		<br />
+		<input type="submit" />
+		</p>
+		</form>
+		</div>
+	);
+
+}
