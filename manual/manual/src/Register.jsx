0d440540c4c91809f9cd0edad686f3ea86793c37
acarrillo13 <a690c665@home.ku.edu>
2025-11-30 20:53:15 -0600

    fix

--- manual/manual/src/Register.jsx
+++ manual/manual/src/Register.jsx
@@ -4,24 +4,24 @@ import { useNavigate } from 'react-router-dom'
 
 function Register() {
     const navigate = useNavigate();
-    return (
-        <div>
-            <h1>Register</h1>
-            <div className="input">
-            <input type="text" placeholder="Username" />
-            </div>
-            <div className="input">
-            <input type="password" placeholder="Password" />
-            </div>
-                    <div className="input">
-            <input type="password" placeholder="Re-enter Password" />
-            </div>
-            <div>
-            <button onClick={() => navigate('/login')}>Submit</button>
-
-            </div>
+  return (
+    <div>
+        <h1>Register</h1>
+        <div className="input">
+          <input type="text" placeholder="Username" />
         </div>
-    );
+        <div className="input">
+          <input type="password" placeholder="Password" />
+        </div>
+                <div className="input">
+          <input type="password" placeholder="Re-enter Password" />
+        </div>
+        <div>
+          <button onClick={() => navigate('/login')}>Submit</button>
+
+        </div>
+    </div>
+  );
 }
 
 export default Register
