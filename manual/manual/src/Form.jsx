f10312f44accce6f5aa3f45dcc1a91a5b62bdaa8
adcarrillo13 <a690c665@ku.edu>
2025-12-07 23:59:05 -0600

    yeah2S

--- manual/manual/src/Form.jsx
+++ manual/manual/src/Form.jsx
@@ -2,7 +2,12 @@ import { useState } from 'react'
 
 function Form() {
   return (
- <></>
+    <div>
+        <h1>Form</h1>
+        <div>
+          <button>New Post</button>
+        </div>
+    </div>
   );
 }
 
