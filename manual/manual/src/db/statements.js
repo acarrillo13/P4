6bdb0c1eb5069a709c921d14fab715c5f4010e5d
acarrillo13 <a690c665@home.ku.edu>
2025-12-11 00:51:19 -0600

    ITS WORKING

--- manual/manual/src/db/statements.js
+++ manual/manual/src/db/statements.js
@@ -1,9 +1,7 @@
-const Database = require('better-sqlite3');
+import Database from 'better-sqlite3';
 const db = new Database('users.db');
 
-const insertUser = (username, password) => {
+export const insertUser = (username, password) => {
     const stmt = db.prepare('INSERT INTO users (username, password) VALUES (?, ?)');
     return stmt.run(username, password);
 };
-
-module.exports = { insertUser };
