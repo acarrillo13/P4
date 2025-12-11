e1cc365da990ae582ae61749ff73f88704a316e8
acarrillo13 <a690c665@home.ku.edu>
2025-12-10 22:37:51 -0600

    idk what I'm doing i got a test tomorrow

--- manual/manual/src/Form.jsx
+++ manual/manual/src/Form.jsx
@@ -2,12 +2,39 @@ import { useState } from 'react'
 import axios from 'axios'
 
 function Form() {
+  const [posts, setPosts] = useState([]);
+  const [text, setText] = useState("");
+  
+  const addPost = (e) => {
+    e.preventDefault();
+    if (!text) return;
+    setPosts([...postMessage, {id:Date.com(), text}])
+    setText("");
+  }
+
+  const deletePost = (id) => {
+    setPosts(postMessage.filter((post) => post.id !== id));
+  }
+
   return (
     <div>
         <h1>Form</h1>
         <div>
-          <button>New Post</button>
+          <form onSubmit={addPost}>
+            <input
+              value={text}
+              onChange={(e) => setText(e.target.value)}
+            />
+            <button>Create</button>
+          </form>
+          {postMessage.map(p=> (
+            <div key={p.id}>
+              {p.text}
+              <button onClick={() => deletePost(p.id)}>Delete</button>
+            </div>
+          ))}
         </div>
+
     </div>
   );
 }
